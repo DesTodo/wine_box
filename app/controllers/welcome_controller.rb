b13f79388e98485cb61a79e51eb33b650b5613cc
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-22 15:59:28 -0600

    update nav-bar to have wine list and about wine fix links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,8 @@ class WelcomeController < ApplicationController
   def homepage
     render :index
   end
+
+  def about
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   root "welcome#homepage"
+  get '/about', to: "welcome#about"
 
   get '/clients/:id/dashboard', to: 'users#index'
+
 
   resources :wines, only: [:index]
 
