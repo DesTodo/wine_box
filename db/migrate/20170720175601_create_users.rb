52a04c757d675a8f79beca4096d6f1459c29c924
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-22 20:59:24 -0600

    remove default value from migration file -- did not create objects with default value

--- db/migrate/20170720175601_create_users.rb
+++ db/migrate/20170720175601_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.text :username
-      t.integer :role, default: 1, null: false
+      t.integer :role
       t.text :password_digest
 
       t.timestamps
