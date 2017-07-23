ad9f65ee3f5a1d6f6f55eb5a22945e5b9ff87549
Desislava Todorova <dslavatodorova@gmail.com>
2017-07-23 16:44:12 -0600

    update model spec for wine to account for added attributes

--- spec/factories/wines.rb
+++ spec/factories/wines.rb
@@ -2,9 +2,12 @@ FactoryGirl.define do
   factory :wine do
     name "MyText"
     variety "MyText"
-    price 1
-    label "MyText"
+    price 100
+    points 89
+    designation "MyText"
     maker "MyText"
     description "MyText"
+    country "MyText"
+    nickname "MyText"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "can be created as subscriber" do
-    subscriber = create(:user, role: 1)
+  describe "users can have different roles" do
+    it "can be created as client" do
+      client = create(:user, role: 2)
 
-    expect(subscriber.role).to eq("subscriber")
-    expect(subscriber.subscriber?).to be_truthy
+      expect(client.role).to eq("client")
+      expect(client.client?).to be_truthy
+    end
+
+
+    it "user is created with role subscriber by default" do
+      default = create(:user)
+
+      expect(default.role).to eq("subscriber")
+      expect(default.subscriber?).to be_truthy
+    end
   end
 
-
-  it "is created as subscriber by default" do
-    default = create(:user)
-
-    expect(default.role).to eq("subscriber")
-    expect(default.subscriber?).to be_truthy
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_uniqueness_of(:username) }
   end
 end
